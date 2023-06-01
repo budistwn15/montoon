8c0729ea8a58de287e3ca09a627aae193d8df5ea
Budi Setiawan <budi.setiawan112001@gmail.com>
2023-06-01 22:14:33 +0700

    feat: change title dynamic

--- resources/js/Pages/Prototype/Login.jsx
+++ resources/js/Pages/Prototype/Login.jsx
@@ -1,18 +1,20 @@
 import TextInput from "@/Components/TextInput.jsx";
 import InputLabel from "@/Components/InputLabel.jsx";
 import PrimaryButton from "@/Components/PrimaryButton.jsx";
-import {Link} from "@inertiajs/react";
+import {Head, Link} from "@inertiajs/react";
 import SecondaryButton from "@/Components/SecondaryButton.jsx";
 
 export default function Login(){
-    return <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
-        <div className="fixed top-[-50px] hidden lg:block">
-            <img src="/images/signup-image.png"
-                 className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt=""/>
-        </div>
-        <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
-            <div>
-                <img src="/images/moonton-white.svg" alt=""/>
+    return <>
+        <Head title="Sign in"/>
+        <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+            <div className="fixed top-[-50px] hidden lg:block">
+                <img src="/images/signup-image.png"
+                     className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt=""/>
+            </div>
+            <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+                <div>
+                    <img src="/images/moonton-white.svg" alt=""/>
                     <div className="my-[70px]">
                         <div className="font-semibold text-[26px] mb-3">
                             Welcome Back
@@ -27,7 +29,7 @@ export default function Login(){
                             <div>
                                 <InputLabel htmlFor="email" value="Email"/>
                                 <TextInput type="email" name="email"
-                                       placeholder="Email Address" variant="primary"/>
+                                           placeholder="Email Address" variant="primary"/>
                             </div>
                             <div>
                                 <InputLabel htmlFor="password" value="Password"/>
@@ -49,7 +51,8 @@ export default function Login(){
                             </Link>
                         </div>
                     </form>
+                </div>
             </div>
         </div>
-    </div>
+    </>
 }
