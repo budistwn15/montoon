e9753b25d0c6cf3896d36644d4ca8c38a7a22e12
Budi Setiawan <budi.setiawan112001@gmail.com>
2023-06-01 21:36:41 +0700

    feat: slicing primary button component

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -1,11 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+PrimaryButton.propTypes = {
+    type: PropTypes.oneOf(["button","submit","reset"]),
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    childer: PropTypes.node
+};
+
 export default function PrimaryButton({ className = '', disabled, children, ...props }) {
     return (
         <button
             {...props}
             className={
-                `inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
-                } ` + className
+                `rounded-2xl border border-white py-[13px] text-center w-full ${disabled && "opacity-25"} ${className}`
             }
             disabled={disabled}
         >
